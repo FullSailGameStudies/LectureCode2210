84fe09c82be7db6b524bc0f8a9eff1f85523106a
Garrett Girod <girodg@hotmail.com>
2022-09-26 10:28:56 -0400

    DrawBlock

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -6,6 +6,30 @@ namespace Day01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Gotham! I am the hero you need.");
+
+            int x = Console.WindowWidth / 2;
+            int y = Console.WindowHeight / 2;
+            DrawBlock(x, y);
+            DrawBlock(10, 25);
+            DrawBlock(x + 2, y - 3);
+            Console.ReadKey();
+        }
+        //
+        // Modify DrawBlock and give it 2 parameters: x,y
+        // use those values instead of the hardcoded values
+        //
+        static void DrawBlock(int x, int y)
+        {
+            //int x = Console.WindowWidth / 2;
+            //int y = Console.WindowHeight / 2;
+
+            Console.SetCursorPosition(x, y);
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+
+            Console.Write("     ");
+
+            Console.ResetColor();
         }
 
     }
