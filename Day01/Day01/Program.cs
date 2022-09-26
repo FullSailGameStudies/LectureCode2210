6091ec0a855ef3d75a4945cefb7feadcebe1bab5
Garrett Girod <girodg@hotmail.com>
2022-09-26 10:44:19 -0400

    factor, ref param

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -8,12 +8,27 @@ namespace Day01
         {
             Console.WriteLine("Hello Gotham! I am the hero you need.");
 
-            int x = Console.WindowWidth / 2;
-            int y = Console.WindowHeight / 2;
-            DrawBlock(x, y);
+            int x1 = Console.WindowWidth / 2;
+            int y1 = Console.WindowHeight / 2;
+            DrawBlock(x1, y1);
             DrawBlock(10, 25);
-            DrawBlock(x + 2, y - 3);
+            DrawBlock(x1 + 2, y1 - 3);
+
+            int sum = 0;
+            int factor = Factor(3,ref sum);
+            Console.WriteLine($"3 * {factor} = {sum}");
             Console.ReadKey();
+        }
+
+        // create a method to randomly generate x,y
+        // use ref parameters to return the x,y
+
+        static int Factor(int num,ref int result)
+        {
+            Random random = new Random();
+            int factor = 3;// random.Next(10);
+            result = num * factor;
+            return factor;
         }
         //
         // Modify DrawBlock and give it 2 parameters: x,y
