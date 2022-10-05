b565257120891cd7e8d4cb52a476ac242684f9d3
Garrett Girod <girodg@hotmail.com>
2022-10-05 10:56:55 -0400

    Reading CSV

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -9,6 +9,7 @@ namespace Day04
     {
         static void Main(string[] args)
         {
+            #region Writing CSV
             //1. Open the file in write mode
             string filePath = @"C:\temp\2210\data.csv";
             char delimiter = '$';
@@ -23,7 +24,42 @@ namespace Day04
                 sw.Write(delimiter);
                 sw.Write(true);
             }//3. CLOSES THE FILE!!
+            #endregion
 
+            #region Reading CSV
+            ReadFile(filePath);
+            #endregion
+        }
+
+        private static void ReadFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                //1. open the file in read mode
+                //using (StreamReader sr = new StreamReader(filePath))
+                {
+                    //string line;
+                    //while((line = sr.ReadLine()) != null)
+
+                    //don't need a using statement if using File.ReadAllText
+                    string line = File.ReadAllText(filePath);//open,read,close the file
+                    {
+                        Console.WriteLine(line);
+                        string[] data = line.Split('$');
+                        string name = data[0];
+                        int num = int.Parse(data[1]);
+                        double dNum = double.Parse(data[2]);
+                        bool isGood = bool.Parse(data[3]);
+
+                        Console.WriteLine(name);
+                        Console.WriteLine(num);
+                        Console.WriteLine(dNum);
+                        Console.WriteLine(isGood);
+                    }
+                }
+            }
+            else
+                Console.WriteLine($"{filePath} does not exists!!");
         }
     }
 }
