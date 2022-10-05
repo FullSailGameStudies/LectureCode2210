be18494011bb5430b5b41b1598a0612c0f972351
Garrett Girod <girodg@hotmail.com>
2022-10-05 11:45:47 -0400

    Serialize/Deserialize

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace Day04
 {
+    enum Superpower
+    {
+        Speed, Money, Strength, Flying, Invisibility, Telekenisis, Swimming
+    }
+    class Superhero
+    {
+        public string Name { get; set; }
+        public string Secret { get; set; }
+        public Superpower Power { get; set; }
+        public int Level { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -29,6 +42,57 @@ namespace Day04
             #region Reading CSV
             ReadFile(filePath);
             #endregion
+
+            List<Superhero> supers = new List<Superhero>();
+            supers.Add(new Superhero() { Name = "Batman", Secret = "Bruce Wayne", Power = Superpower.Money, Level = 50 });
+            supers.Add(new Superhero() { Name = "Superman", Secret = "Clark Kent", Power = Superpower.Flying, Level = 10 });
+            supers.Add(new Superhero() { Name = "Flash", Secret = "Barry Allen", Power = Superpower.Speed, Level = 25 });
+            supers.Add(new Superhero() { Name = "Aquaman", Secret = "Fish Guy", Power = Superpower.Swimming, Level = 30 });
+            supers.Add(new Superhero() { Name = "Wonder Woman", Secret = "Diana Prince", Power = Superpower.Strength, Level = 40 });
+
+            Serialize(supers, filePath);
+            List<Superhero> JLA = Deserialize(filePath);
+
+            foreach (var super in JLA)
+            {
+                Console.WriteLine($"Hello citizen. I am {super.Name} (aka {super.Secret}). I am good at {super.Power}!");
+            }
+        }
+
+        private static List<Superhero> Deserialize(string filePath)
+        {
+            List<Superhero> supers = null;
+            filePath = Path.ChangeExtension(filePath, ".json");
+            if (File.Exists(filePath))
+            {
+                string jsonText = File.ReadAllText(filePath);
+                //jsonText = "Steev";
+                Console.WriteLine(jsonText);
+
+                //deserialize the text into objects
+                try
+                {
+                    supers = JsonConvert.DeserializeObject<List<Superhero>>(jsonText);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return supers;
+        }
+
+        private static void Serialize(List<Superhero> supers, string filePath)
+        {
+            filePath = Path.ChangeExtension(filePath, ".json");
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                using (JsonTextWriter jtw = new JsonTextWriter(sw))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Formatting = Formatting.Indented;
+                    serializer.Serialize(jtw, supers);
+                }
+            }
         }
 
         private static void ReadFile(string filePath)
